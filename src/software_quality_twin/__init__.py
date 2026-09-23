e53b9ca424fb39427b559ac5492d3a46acc09790
Hira Khyzer <55583242+Hirakhyzer@users.noreply.github.com>
2026-09-23 16:44:39 +0800

    Export telemetry evidence assurance utilities

--- src/software_quality_twin/__init__.py
+++ src/software_quality_twin/__init__.py
@@ -1,5 +1,10 @@
 """Software Quality Digital Twin Lab research package."""
 
+from .evidence_assurance import (
+    TelemetryAssurance,
+    apply_telemetry_assurance,
+    assess_telemetry_assurance,
+)
 from .schema import (
     DriftAssessment,
     QualityState,
@@ -18,4 +23,7 @@ __all__ = [
     "DriftAssessment",
     "TrendAssessment",
     "RiskLevel",
+    "TelemetryAssurance",
+    "assess_telemetry_assurance",
+    "apply_telemetry_assurance",
 ]
